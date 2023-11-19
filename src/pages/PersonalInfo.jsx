677776608f06a5cc41f5a2c59bc581c26464e72c
Ninunutsi <khurtsidzenino4@gmail.com>
2023-11-19 23:12:23 +0400

    sod

--- src/pages/PersonalInfo.jsx
+++ src/pages/PersonalInfo.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import useLocalStorage from "../hooks/useLocalStorage";
 
